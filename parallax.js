b13138cae972cf131c6fc1985ad68c77f9135458
PeregrinTuk <julioagustinmayorga@gmail.com>
2018-04-13 10:08:40 -0300

    Delete parallax.js

--- parallax.js
+++
@@ -1,7 +0,0 @@
-var ypos,image;
-function parallex() {
-    ypos = window.pageYOffset;
-    image = document.getElementById("image");
-    image.style.top = ypos * .4 + "px";
-}
-window.addEventListener("scroll", parallex);
